f19865cd355534fca8031aba5256ede4844409dd
paul <birnbaumpaul@yahoo.fr>
2022-04-25 10:33:44 +1100

    Nft component fixed

--- components/Nft.js
+++ components/Nft.js
@@ -21,15 +21,9 @@ export default function Nft({ user }) {
         })
     }, [])
 
-    console.log(data.total);
-
-
-
     return (
         <CustomContainer>
-
-
-            <Text fontSize="xl" fontWeight="bold">{`My Nfts ( ${data.total} ) `}</Text>
+            <Text fontSize="xl" fontWeight="bold">{`My Nfts  `}</Text>
             {data && data.result.map(nft => (
                 <Box key={nft.token_hash} mt='4' px='2' py='2' borderWidth="1px" borderRadius="md">
                     <a href={`${BASE_URL_OPENSEA}${nft.token_address}/${nft.token_id}`} target="_blank" rel="noreferrer noopener">⛵&nbsp;Click to see on Open Sea</a>
